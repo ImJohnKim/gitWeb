55151264c6970e137723864e18823802152ac048
ImJohnKim <johnkim@johnkimprof.com>
2018-05-04 20:01:50 +0900

    header complete

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,10 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".features-item"), "85%");
 new RevealOnScroll($(".testimonials"), "60%");
+
+var stickyHeader = new StickyHeader();
