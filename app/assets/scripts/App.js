d0c32bbe8d477c0359f7d93a8d0cf182c3a8387e
ImJohnKim <johnkim@johnkimprof.com>
2018-05-05 12:03:05 +0900

    Modal finished

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,10 +1,11 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".features-item"), "85%");
 new RevealOnScroll($(".testimonials"), "60%");
-
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
